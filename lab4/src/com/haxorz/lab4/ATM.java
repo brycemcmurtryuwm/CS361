7d2c1b43022b7c2ab5f8c2731ed13d9b99b5c759
THATSURFACE707\ThatG <lopez35@uwm.edu>
2018-02-13 11:25:52 -0600

    lab4

---
+++ lab4/src/com/haxorz/lab4/ATM.java
@@ -0,0 +1,13 @@
+package com.haxorz.lab4;
+
+public class ATM {
+
+    //start method
+
+    //execute method
+}
+
+
+//tests
+    //console input
+    //units tests
---
+++ lab4/src/com/haxorz/lab4/Account.java
@@ -0,0 +1,10 @@
+package com.haxorz.lab4;
+
+public class Account {
+
+    //acctNum
+    //PIN Code
+    //balance
+
+    //validate (correct PIN?)
+}
