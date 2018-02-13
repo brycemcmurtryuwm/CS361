908f8c66d512951c7bcb93235e5a5efd1a1b7b0e
BRYCE-MCMURTRY\drema <mcmurtr5@uwm.edu>
2018-02-13 12:11:56 -0600

    started bank

--- lab4/src/com/haxorz/lab4/Bank.java
+++ lab4/src/com/haxorz/lab4/Bank.java
@@ -1,6 +1,11 @@
 package com.haxorz.lab4;
 
 public class Bank {
-
-    //validate(account)
+	
+	private Account 1234 = new Account();
+	private Account 6789 = new Account();
+	//validate(account)
+	public boolean validate(Account) {
+		
+	}
 }
