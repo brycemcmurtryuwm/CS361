ff829d643ad0e399ae165c848d2c775cffdc2951
smit2633 <smit2633@uwm.edu>
2018-02-20 12:28:38 -0600

    The printer

--- lab5/src/com/haxorz/lab5/Printer.java
+++ lab5/src/com/haxorz/lab5/Printer.java
@@ -1,9 +1,18 @@
 package com.haxorz.lab5;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
 
 public class Printer {
 
-    public String print(String toPrint, boolean inlcudeTimeStamp){
+    public String print(String toPrint, boolean includeTimeStamp){
         //<time> <transaction> <amount>
+    	final SimpleDateFormat sdf = new SimpleDateFormat("HH.mm.ss");
+    	Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+    	
+    	if(includeTimeStamp == false){
+    		return toPrint;
+    	}
+    	return sdf.format(timestamp) + " " + toPrint;
     }
 
 }
