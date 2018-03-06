c9b3e511a3be8f395fca26c18ed9ebe9b2a0bc3d
THATSURFACE707\ThatG <lopez35@uwm.edu>
2018-03-06 11:02:10 -0600

    added tests back to repository

--- ChronoTimer/Tests/Commands/Parsing.java
+++ ChronoTimer/Tests/Commands/Parsing.java
@@ -1,6 +1,48 @@
 package Commands;
+import com.haxorz.ChronoTimer.Commands.*;
+import com.sun.org.apache.bcel.internal.generic.INSTANCEOF;
+import org.junit.*;
 
 public class Parsing {
 
 
+	@Before
+	public void before() {
+	}
+
+	@Test
+	public void testPower() {
+		CTCommand c =CTCommand.ParseFromString("10:00:00 Start");
+		Assert.assertTrue(c instanceof GenericCmd);
+	}
+	@Test
+	public void testBadCommand() {
+		CTCommand c =CTCommand.ParseFromString("fghjkl");
+		Assert.assertTrue(c == null);
+	}
+	@Test
+	public void testWithoutStamp(){
+		CTCommand c =CTCommand.ParseFromString("cancel");
+		Assert.assertTrue(c instanceof CancelCmd);
+	}
+	@Test
+	public void testEvent(){
+		CTCommand c =CTCommand.ParseFromString("21:10:10 Event IND");
+		Assert.assertTrue(c instanceof EventCmd);
+	}
+	@Test
+	public void testBadTime(){
+		try{
+			CTCommand c =CTCommand.ParseFromString("25:10:10 Event IND");
+			Assert.assertTrue(true);
+		}
+		catch(Exception e){
+			Assert.fail();
+		}
+	}
+	@Test
+	public void testPrint(){
+		CTCommand c =CTCommand.ParseFromString("21:10:10 Print hello");
+		Assert.assertTrue(c instanceof PrintCmd);
+	}
 }
